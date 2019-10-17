581bfb421fcc8fa58e256f231a88f3a8d7656919
iamlegolas <muhammadbintahirmir@gmail.com>
2019-10-17 18:33:43 -0400

    Made a small addition to autoRigger.project.py
    Consequence of setting up IDE env on ETC work PC

--- python/autoRigger/project.py
+++ python/autoRigger/project.py
@@ -3,4 +3,5 @@ main project file with central variables
 """
 
 scene_scale = 2.0
-main_project_path = '/Users/muhammadbintahirmir/git/fall19_autorigger/assets'
+#main_project_path = '/Users/muhammadbintahirmir/git/fall19_autorigger/assets' #MAC
+main_project_path = 'C:/Users/mmir/git/fall19_autorigger/assets' #ETC_PC
